ff29bc6b86dca27eddccb94c4332610bb7516206
KIM <youngseo914@gmail.com>
2023-05-30 22:20:28 +0900

    file commit.

--- src/main/java/com/example/zerobasestudy23/studyex.java
+++ src/main/java/com/example/zerobasestudy23/studyex.java
@@ -1,27 +1,32 @@
 package com.example.zerobasestudy23;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-public class studyex {
+import java.net.URI;
 
+class studyex
+{
 
-    public static void main(String[] args) throws Exception {
-        URL url = new URL("http://openapi.seoul.go.kr:8088/6b47795770647564313035424e485451/json/TbPublicWifiInfo/1/6/");
-        URLConnection connection = url.openConnection();
+        public static void main(String[] args) throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpUriRequest httpget = RequestBuilder.get()
+                    .setUri(new URI("http://openapi.seoul.go.kr:8088/6b47795770647564313035424e485451/json/TbPublicWifiInfo/1/6/"))
+                    .addParameter("page", "1")
+                    .build();
 
-        // `HttpURLConnection` connection = (HttpURLConnection) url.openConnection();
-        // connection.setRequestMethod("GET");
-
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(connection.getInputStream()))) {
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } finally {
+                response.close();
             }
+        } finally {
+            httpclient.close();
         }
     }
-}
-
-
+    }
